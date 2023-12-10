149882feee53a5bb7fb5eb205ac6a6b16d86694a
RotemDevAcc <guyron2000@gmail.com>
2023-12-10 02:38:27 +0200

    Fixed the static dir

--- project/settings.py
+++ project/settings.py
@@ -166,10 +166,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
